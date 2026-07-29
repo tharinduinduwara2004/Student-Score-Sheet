5205e13e4571d7b23990c2145c02d6b2fdb6f621
tharinduinduwara2004 <tinduwara427@gmail.com>
2026-07-30 02:19:55 +0530

    fix: correct variable names and improve data handling in addData function

--- script.js
+++ script.js
@@ -23,3 +23,44 @@ function closeDialog(){
         dialogContainer.style.display = 'none';
     }, 100);
 }
+
+const studenta = [];
+
+function addData(){
+    let name = nameInput.value;
+    let score = score = scoreInput.value;
+    if(nmae != '' && score != ''){
+        students.push(
+            {
+                name: name,
+                score: score,
+            }
+        );
+        nameInput.value = '';
+        scoreInput.value = '';
+    }else{
+        alert("Please fill all the fields.");
+    }
+}
+
+addDataBtn.onclick = addData;
+
+//display list
+function displayList(){
+    students.forEach(function(student){
+        let record = document.createElement('tr');
+        let td0 = document.createElement('td');
+        td0.innerHTML = students.indexOf(student);
+        let td1 = document.createElement('td');
+        td1.innerHTML = student.name;
+        let td2 = document.createElement('td');
+        td2.innerHTML = student.score;
+        let td3 = document.createElement('td');
+
+        record.appendChild(td0);
+        rocord.appendChild(td1);
+        record.appendChild(td2);
+        record.appendChild(td3);
+        list.appendChild(record);
+    })
+}
